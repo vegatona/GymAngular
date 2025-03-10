f8daae1dcc569ddbf27836736273cf78bef13d6f
vegatona <oswaldovega756@gmail.com>
2025-03-10 09:44:26 -0700

    primer commit

---
+++ branches/branches.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branches',
+  imports: [],
+  templateUrl: './branches.component.html',
+  styleUrl: './branches.component.css'
+})
+export class BranchesComponent {
+
+}
